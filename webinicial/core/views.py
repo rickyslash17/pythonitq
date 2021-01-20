ef3d1d2a7cfea3411f581ac7f4c821ff3ba80dc4
Alexander9595 <59055640+Alexander9595@users.noreply.github.com>
2021-01-19 20:11:26 -0500

    Update views.py

--- webinicial/core/views.py
+++ webinicial/core/views.py
@@ -5,22 +5,12 @@ html_cabecera ="""
 <h1>Welcome</h1>
 <ul>
 <li><a href="/">Home</li>
-<li><a href="/presentacion">Presentación</li>
-<li><a href="/contacto">Contacto</li>
-<li><a href="/catalogo">Catalogo</li>
+<li><a href="/presentacion">Presentación Producto</li>
 </ul>
 
 """
 
 # Create your views here.
-def home(request):
-    return HttpResponse(html_cabecera+ "<h1>Proyecto</h1>")
 
-def presentacion(request):
-    return HttpResponse(html_cabecera+"<h1>Mi nombre es Gangale carlos</h1>")
-
-def contacto(request):
-    return HttpResponse(html_cabecera+"<h1>Numero telefonico 0987158210</h1>")
-
-def catalogo(request):
+def producto(request):
     return HttpResponse(html_cabecera+"<h1>Productos</h1>")
